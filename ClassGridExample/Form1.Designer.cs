01bbda81754ebc59b37a88ea6aab953fee3b12f9
aucan <aucan@AUCAN>
2017-11-23 11:46:11 +0300

    first

--- ClassGridExample/Form1.Designer.cs
+++ ClassGridExample/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ekleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 388);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem,
+            this.jsonToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(642, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ekleToolStripMenuItem
+            // 
+            this.ekleToolStripMenuItem.Name = "ekleToolStripMenuItem";
+            this.ekleToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.ekleToolStripMenuItem.Text = "ekle";
+            this.ekleToolStripMenuItem.Click += new System.EventHandler(this.ekleToolStripMenuItem_Click);
+            // 
+            // jsonToolStripMenuItem
+            // 
+            this.jsonToolStripMenuItem.Name = "jsonToolStripMenuItem";
+            this.jsonToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.jsonToolStripMenuItem.Text = "2 Json";
+            this.jsonToolStripMenuItem.Click += new System.EventHandler(this.jsonToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 412);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jsonToolStripMenuItem;
     }
 }
 
--- ClassGridExample/Form1.cs
+++ ClassGridExample/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web.Script.Serialization;
 
 namespace ClassGridExample
 {
@@ -15,6 +17,68 @@ namespace ClassGridExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class deneme
+        {
+            public deneme(string _a,string _b, int _c, int _d)
+            {
+                a = _a;
+                b = _b;
+                c = _c;
+                d = _d;
+            }
+
+            [DisplayName("kolon 1")]
+            public string a { get; set; }
+
+            [DisplayName("b")]
+            public string b { get; set; }
+
+            [DisplayName("c")]
+            public int c { get; set; }
+
+            [DisplayName("id")]
+            public int d { get; set; }
+        }
+
+        List<deneme> liste = new List<deneme>();
+        int satir = 0; 
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetDataGrid();
+        }
+
+        private void SetDataGrid()
+        { 
+            dataGridView1.Columns.Add("a", "Kolon 1");
+            dataGridView1.Columns.Add("b", "b");
+            dataGridView1.Columns.Add("c", "c");
+            dataGridView1.Columns.Add("d", "id");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(300); 
+                liste.Add(new deneme("a", "b", 1, ++satir));
+                //dataGridView1.Invoke((Action)(() => dataGridView1.Rows.Add("a", "b", 1, satir)));
+                dataGridView1.Invoke((Action)(() => dataGridView1.Rows.Insert(0,"a", "b", 1, satir)));
+            }
+        } 
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void jsonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var tmp = liste;
+            string json = new JavaScriptSerializer().Serialize(tmp);
+            MessageBox.Show(json);
         }
     }
 }
